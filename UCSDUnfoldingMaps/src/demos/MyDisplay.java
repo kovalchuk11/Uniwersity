779dd21ac4d8b6469e318a9eebc40af2f01962dc
kovalchuk11 <kovalchuk427@gmail.com>
2019-04-14 21:28:19 +0300

    new

--- UCSDUnfoldingMaps/src/demos/MyDisplay.java
+++ UCSDUnfoldingMaps/src/demos/MyDisplay.java
@@ -14,6 +14,7 @@ public class MyDisplay extends PApplet {
 
 	public void setup()
 	{
+		frameRate(60);
 		size(400, 400);
 		background(200, 200, 200);
 		
@@ -21,14 +22,14 @@ public class MyDisplay extends PApplet {
 	
 	public void draw()
 	{
-		fill(255, 255, 0);
+		fill(255, 0, 0);
 		ellipse(200, 200, 390, 390);
 		fill(0, 0, 0);
 		ellipse(120, 130, 50, 70);
 		ellipse(280, 130, 50, 70);
 		
 		noFill();
-		arc(200, 280, 75, 75, 0, PI);
+		arc(200, 280, 75, 175, 0, PI);
 	}
 	
 }
